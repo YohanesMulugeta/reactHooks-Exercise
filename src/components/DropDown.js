cb16d940695f2ffa1694894c51c7b87e75c576ac
YohanesMulugeta <yohanesmulugeta21@gmail.com>
2022-04-27 13:57:15 +0300

    a special tutor ref used to compare and do something

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,12 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    // console.log(ref.current);
+
+    document.querySelector("body").addEventListener(
+      "click",
+      function (e) {
+        // guard key that is if e.target is descendant of the ref.current this handler wont do a shit
+        if (ref.current.contains(e.target)) return;
+
+        setOpen(false);
+        // console.log("body clicked");
+      },
+      { capture: true }
+    );
+  }, []);
+
+  // document.querySelector("body").addEventListener("click", (e) => {
+  //   setOpen(!open);
+  //   console.log(e.target);
+  // });
+
+  ////////////////////////////////////////////      MAPING THE OPTIONS IN TO LISTS
+  ////////////////////////////////////////////
+
   const renderOptions = options.map((opt, i) => {
+    if (opt.value === selected.value) return null; // null in react means render nothing
+
     return (
       <div
         key={opt.value}
         className="item"
         onClick={() => {
+          // console.log("item clicked");
           onSelectedChange(opt);
         }}
       >
@@ -15,14 +45,25 @@ export default ({ options, selected, onSelectedChange }) => {
     );
   });
 
+  /////////////////////////////////////////     THE WILL BE RENDERED VALUE
+  ////////////////////////////////////////
+
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="ui field">
         <label className="label">Selection</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={(e) => {
+            // console.log("div clicked");
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderOptions}
+          </div>
         </div>
       </div>
     </div>
