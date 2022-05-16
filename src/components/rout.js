bb434668cfe044e87b394a47c55d7e74c3ce5ab9
YohanesMulugeta <yohanesmulugeta21@gmail.com>
2022-05-16 13:32:55 +0300

    hooks ende addis

--- src/components/rout.js
+++
@@ -1,5 +0,0 @@
-const Rout = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
-};
-
-export default Rout;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 // RENDERING the App component
 const container = document.getElementById("root");
 const root = createRoot(container);
