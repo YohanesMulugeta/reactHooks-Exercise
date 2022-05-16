51c75b316e766b81c975a438fe8fb91244bdfb25
YohanesMulugeta <yohanesmulugeta21@gmail.com>
2022-05-16 13:57:36 +0300

    the items array is passed as a prop to the accordion

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Accordion from "./Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front-end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite js liberary among engineers",
+  },
+  {
+    title: "How to use React?",
+    content: "You use React by creating components",
+  },
+];
+
 const App = () => {
   return (
     <div className="">
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
