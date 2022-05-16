4dc863da0bbd7668a68a3c44c6949155cbb061c2
YohanesMulugeta <yohanesmulugeta21@gmail.com>
2022-05-16 23:06:46 +0300

    the use effect is working properly with best practice of using an async fuction inside a useeffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
   const [result, setResult] = useState("");
-  return <h1>Search</h1>;
+
+  useEffect(() => {
+    const find = async () => {
+      const response = axios.get("https://en.wikipedia.org/w/api.php?", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      console.log(response);
+    };
+
+    find();
+  }, [term]);
+  //   console.log(term);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            className="input"
+            onChange={(e) => {
+              // insuring CONTROLLED data transfer
+              setTerm(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
