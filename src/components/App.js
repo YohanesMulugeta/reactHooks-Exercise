38341b5116bf70918ed3f69ced9fbac18edbea5d
YohanesMulugeta <yohanesmulugeta21@gmail.com>
2022-05-16 13:50:51 +0300

    accordion is connected to the App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Accordion from "./Accordion";
+
+const App = () => {
+  return (
+    <div className="">
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
