86d1215885a8b5b49c7d55d9f01e7bec48741f30
YohanesMulugeta <yohanesmulugeta21@gmail.com>
2022-05-17 10:46:43 +0300

    dropDown working using useEffect with some clean up

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Dropdown = ({ options, title, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (!e.target.closest(".dropi") && open) setOpen(!open);
+    };
+
+    document.body.addEventListener("click", handler);
+
+    return () => document.body.removeEventListener("click", handler);
+  });
+
   const renderedOptions = options.map((option) => {
     // KEY GUARD....
-    if (option.value === selected.value) return;
+    if (option.value === selected.value) return null;
 
     return (
       <div
@@ -11,6 +23,7 @@ const Dropdown = ({ options, title, selected, onSelectedChange }) => {
         className="item"
         onClick={() => {
           // ====================================== SENDING DATA from child to parent
+          // console.log("item");
           onSelectedChange(option);
         }}
       >
@@ -20,13 +33,21 @@ const Dropdown = ({ options, title, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div className="ui form dropi">
       <div className="field">
         <label className="label">{title}</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => {
+            console.log("paren");
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
